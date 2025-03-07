30e379a1eb8fe883ec0232784996dc871b4ae327
mohammad <taskforcemmd@gmail.com>
2025-03-07 13:38:52 +0330

    Add project to the github 3/7/2025

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+function hidesidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'none'
+}
+
+function showsidebar() {
+    const sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'flex'
+}
